7a098f43dfb7b70a5ca16494f5afac0c4bd233f3
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-03-05 21:32:01 +0900

    App - 글생성 UI 만들기

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var http=require('http');
-var fs=require('fs');
-var url=require('url');
-  function templateHTML(title, list, body){
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function templateHTML(title, list, body) {
     return `
     <!doctype html>
     <html>
@@ -12,83 +13,76 @@ var url=require('url');
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
+      <a href="/create">create</a>
       ${body}
     </body>
     </html>
     `;
-  }
-  function templateList(filelist){
+}
+
+function templateList(filelist) {
     var list = '<ul>';
     var i = 0;
-    while(i < filelist.length){
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
+    while (i < filelist.length) {
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        i = i + 1;
     }
-    list = list+'</ul>';
+    list = list + '</ul>';
     return list;
-  }
-var app=http.createServer(function(request,response){
-  var _url=request.url;///?=HTML 이부분 가져옴
-  var queryData = url.parse(_url,true).query;// querystring parse 결과:{id=HTML}
-  var pathname=url.parse(_url,true).pathname;
+}
+var app = http.createServer(function(request, response) {
+    var _url = request.url; ///?=HTML 이부분 가져옴
+    var queryData = url.parse(_url, true).query; // querystring parse 결과:{id=HTML}
+    var pathname = url.parse(_url, true).pathname;
 
-  if (pathname === '/')
-  {
-    if (queryData.id===undefined)//존재하지 않을때
-      {
-        //fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
-          //특정 디렉터리에서 파일을 읽어서,description이라는 변수를 생성해주는 코드
-          fs.readdir('./data',function(error,filelist){
-          //  console.log(filelist);
-            var title='WelCome';
-            var description='Hello,Node.js';
-            var list=templateList(filelist);
-            var template=templateHTML(title,list,`<h2>${title}</h2>${description}`);
-            /*`
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 -${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>`;*/
-            //console.log(__dirname+url);
-            //response.end(fs.readFileSync(__dirname+url));
-            //사용자가 선택한 URL에 따라 1.HTML,2.HTML이런거 읽어옴
-            //respose.end(queryData.id)-> 웹페이지출력은 그 querydata의id값
+    if (pathname === '/') { //home으로 갔느냐.
+        if (queryData.id === undefined) //존재하지 않을때
+        {
+            //fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+            //특정 디렉터리에서 파일을 읽어서,description이라는 변수를 생성해주는 코드
+            fs.readdir('./data', function(error, filelist) {
+                //  console.log(filelist);
+                var title = 'WelCome';
+                var description = 'Hello,Node.js';
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readdir('./data', function(error, filelist) {
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                    var title = queryData.id;
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+        }
+    } else if (pathname === '/create') { //사용자가 서버에게 정보보내기
+        fs.readdir('./data', function(error, filelist) {
+            var title = 'WEB - create';
+            var description = 'Hello,Node.js';
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `
+            <form action="http://localhost:3000/process_create" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                    <textarea name="description"
+                    placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>
+            `);
             response.writeHead(200);
             response.end(template);
-          })
-
-
-
-      //  });
-      } else{
-        fs.readdir('./data',function(error,filelist){
-      //    console.log(filelist);
-          fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
-          var title=queryData.id;
-        //  var description='Hello,Node.js';
-          var list=templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-        //console.log(__dirname+url);
-        //response.end(fs.readFileSync(__dirname+url));
-        //사용자가 선택한 URL에 따라 1.HTML,2.HTML이런거 읽어옴
-        //respose.end(queryData.id)-> 웹페이지출력은 그 querydata의id값
-        response.writeHead(200);
-        response.end(template);
         });
-      });
-      }
-    }else{
-      response.writeHead(404);
-      response.end('Not Found');
+    } else {
+        response.writeHead(404);
+        response.end('Not Found');
     }
 });
 app.listen(3000);
